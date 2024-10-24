6715a2ddf03f4ea7f36ed3c79c7023f76186e74c
a20230167 <a20230167@juan23.edu.pe>
2024-10-23 20:56:17 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+document.write("<h1>Bievenidos a la pagina de JavaScript<h1>");
+var non = prompt("Ingrese su nombre");
+document.write(
+  "Hola " + non + " es tu primer contacto con la programación en JavaScript"
+);
